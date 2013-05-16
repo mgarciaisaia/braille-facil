19e77dfcf4bf703235693a6fcfd5c0a32e4f8db0
Matias Garcia Isaia <mgarciaisaia@gmail.com>
2013-05-16 16:12:20 -0300

    Non-Ascii characters implemented
    See #3
    Still have to handle multi-cell characters

--- braille.js
+++ braille.js
@@ -19,7 +19,7 @@ var alphabet = (function(lettersToCodes){
 			return codesToLetters[code];
 		}
 	};
-})({'a':1,'b':3,'c':9,'d':25,'e':17,'f':11,'g':27,'h':19,'i':10,'j':26,'k':5,'l':7,'m':13,'n':29,'o':21,'p':15,'q':31,'r':23,'s':14,'t':30,'u':37,'v':39,'x':45,'y':61,'z':53,'ñ':59});
+})({'a':1,'b':3,'c':9,'d':25,'e':17,'f':11,'g':27,'h':19,'i':10,'j':26,'k':5,'l':7,'m':13,'n':29,'o':21,'p':15,'q':31,'r':23,'s':14,'t':30,'u':37,'v':39,'x':45,'y':61,'z':53,'ñ':59, 'á': 55, 'é': 46, 'í': 12, 'ó': 44, 'ú': 62, 'ü': 51, '&' : 47, '.' : 4, '[vieneNumero]' : 60, '[vieneMayuscula]' : 40 , ',' : 2, '?' : 34, ';' : 6, '!' : 22, '"' : 38, '(' : 35, ')' : 28, '-' : 36, '*' : 20, ' ' : 0}); // FIXME: dual-cell characters
 
 var outputText = (function() {
 	var outputText = document.createElement('textarea');
