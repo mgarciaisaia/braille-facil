60b45652cbd3ddedeb86996f6ba7240a8fede4be
Matias Garcia Isaia <mgarciaisaia@gmail.com>
2013-06-24 19:10:02 -0300

    GPLv3 licensed
    Fixes #20

--- braille.js
+++ braille.js
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2013 BrailleFacil Contributors
+ *
+ * This file is part of BrailleFacil
+ * 
+ * BrailleFacil is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BrailleFacil is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with BrailleFacil.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 Array.prototype.remove = function(element) {
 	var index;
 	while((index = this.indexOf(element)) != -1) {
